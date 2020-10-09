aff12717ca3e3c5d06b8aefce3accc670dd071f2
Deepanshuchauhan123 <deepanshuchauhan7287@gmail.com>
2020-10-09 10:16:51 +0530

    DB Connectivity

---
+++ src/com/deepanshu/DBInitializer.java
@@ -0,0 +1,9 @@
+package com.deepanshu;
+
+public interface DBInitializer 
+{
+	String DRIVER = "com.mysql.jdbc.Driver";
+	String CON_STRING = "jdbc:mysql://localhost:3306";
+	String USERNAME = "root";
+	String PASSWORD = null;
+}
---
+++ src/com/deepanshu/RegisterUser.java
@@ -0,0 +1,39 @@
+package com.deepanshu;
+
+import java.sql.*;
+
+public class RegisterUser 
+{
+	static int status=0 ;
+	
+	public static int register(String username,String password,String repassword,double amount,String address,double phone)
+	{
+		Connection con = GetCon.getcon();
+		PreparedStatement ps;
+		
+		try
+		{
+			
+			ps = con.prepareStatement("insert into NEWACCOUNT values(?,?,?,?,?,?,?)");
+			
+			int nextvalue1 = GetCon.getPrimaryKey();
+			
+			ps.setInt(1,nextvalue1);
+			ps.setString(2, username);
+			ps.setString(3,password);
+			ps.setString(4, repassword);
+			ps.setDouble(5, amount);
+			ps.setString(6, address);
+			ps.setDouble(7, phone);
+			
+			status = ps.executeUpdate();
+			
+			
+		}catch(SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+		return status;
+	}
+}
